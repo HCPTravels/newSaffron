2606b632df72e7cf2c2d11cd3174dddcfc9f8bb9
HCPTravels <bharghavhcptravel@gmail.com>
2025-06-29 03:27:43 +0530

    bug fixed

--- src/pages/As.jsx
+++ src/pages/As.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function As() {
   return (
     <div className='flex items-center justify-center h-screen bg-[#ff6523] text-white text-2xl font-bold'>
-        Hey Baby I Love You So Much Mera Bccha
+        Hey
     </div>
   )
 }
